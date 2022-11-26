1ee2778facc04380005760f15afc8ceb7e3aa451
manishcoforge <manish.26.kumar@coforge.com>
2022-11-26 15:01:13 +0530

    Updates

---
+++ precache-manifest.e3845c6cfeae1dd2babd24b96f93c57f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "62abf8e84f111cd5e1b8",
+    "url": "/website1/static/css/main.44b699d2.chunk.css"
+  },
+  {
+    "revision": "62abf8e84f111cd5e1b8",
+    "url": "/website1/static/js/main.62abf8e8.chunk.js"
+  },
+  {
+    "revision": "315588224ddf1269d585",
+    "url": "/website1/static/js/1.31558822.chunk.js"
+  },
+  {
+    "revision": "ae2ba8064115dc3693ca",
+    "url": "/website1/static/js/runtime~main.ae2ba806.js"
+  },
+  {
+    "revision": "72671cdd3ec425fd8d2aeda49abf2aba",
+    "url": "/website1/static/media/whatmatters.72671cdd.png"
+  },
+  {
+    "revision": "a5af41428d6ee50ef259fcc417d5daf6",
+    "url": "/website1/static/media/topleft1.a5af4142.png"
+  },
+  {
+    "revision": "258cc96f9b7d6baf522096ac00e05b31",
+    "url": "/website1/static/media/whatmattersclients.258cc96f.png"
+  },
+  {
+    "revision": "9f9497e4aaa3af8dc2384921305fe672",
+    "url": "/website1/static/media/whatmatterspeople.9f9497e4.png"
+  },
+  {
+    "revision": "1aef846b29169a7418ed756f3cf8f52f",
+    "url": "/website1/static/media/topright2.1aef846b.png"
+  },
+  {
+    "revision": "254eeb6373969569c627f0abf2d0b7df",
+    "url": "/website1/static/media/bottomleft3.254eeb63.png"
+  },
+  {
+    "revision": "0cce56d3aeeb15371d43ebf2e643c3f0",
+    "url": "/website1/static/media/bottomright4.0cce56d3.png"
+  },
+  {
+    "revision": "fffc966722fbe42a9c0373db3a94483f",
+    "url": "/website1/index.html"
+  }
+];
